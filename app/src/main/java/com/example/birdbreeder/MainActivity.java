84fe958ba0c21cb30a2b9c1c0a6721bcb25366cb
HayaOsama <42942423+HayaOsama@users.noreply.github.com>
2020-12-08 00:34:34 +0200

    themes

--- app/src/main/java/com/example/birdbreeder/MainActivity.java
+++ app/src/main/java/com/example/birdbreeder/MainActivity.java
@@ -1,14 +1,17 @@
 package com.example.birdbreeder;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.ui.AppBarConfiguration;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    private AppBarConfiguration mAppBarConfiguration;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setSupportActionBar(findViewById(R.id.toolbar));
+
     }
 }
