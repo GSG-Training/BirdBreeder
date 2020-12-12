ceb93a2ffc6d6e1a29e15e0ef50d0eb37ef6dc79
HayaOsama <42942423+HayaOsama@users.noreply.github.com>
2020-12-12 23:20:45 +0200

    inner_fragments

--- app/src/main/java/com/example/birdbreeder/MainActivity.java
+++ app/src/main/java/com/example/birdbreeder/MainActivity.java
@@ -1,17 +1,18 @@
 package com.example.birdbreeder;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.ui.AppBarConfiguration;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
     private AppBarConfiguration mAppBarConfiguration;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       setSupportActionBar(findViewById(R.id.toolbar));
+        setSupportActionBar(findViewById(R.id.toolbar));
 
     }
 }
--- app/src/main/java/com/example/birdbreeder/SignUpActivity.java
+++ app/src/main/java/com/example/birdbreeder/SignUpActivity.java
@@ -1,8 +1,8 @@
 package com.example.birdbreeder;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class SignUpActivity extends AppCompatActivity {
 
