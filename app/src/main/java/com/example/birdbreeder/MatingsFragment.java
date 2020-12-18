74381393629bc6f10b8c58775dd13628aa0469b0
HayaOsama <42942423+HayaOsama@users.noreply.github.com>
2020-12-18 21:52:16 +0200

    main_activity_fragments

--- app/src/main/java/com/example/birdbreeder/MatingsFragment.java
+++ app/src/main/java/com/example/birdbreeder/MatingsFragment.java
@@ -1,18 +1,19 @@
 package com.example.birdbreeder;
 
 import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import androidx.fragment.app.Fragment;
-
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link ChatStationFragment#newInstance} factory method to
+ * Use the {@link MatingsFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class ChatStationFragment extends Fragment {
+public class MatingsFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -23,7 +24,7 @@ public class ChatStationFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public ChatStationFragment() {
+    public MatingsFragment() {
         // Required empty public constructor
     }
 
@@ -33,11 +34,11 @@ public class ChatStationFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment ChatStationFragment.
+     * @return A new instance of fragment MatingsFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static ChatStationFragment newInstance(String param1, String param2) {
-        ChatStationFragment fragment = new ChatStationFragment();
+    public static MatingsFragment newInstance(String param1, String param2) {
+        MatingsFragment fragment = new MatingsFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -58,6 +59,6 @@ public class ChatStationFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chat_station, container, false);
+        return inflater.inflate(R.layout.fragment_matings, container, false);
     }
 }
--- app/src/main/java/com/example/birdbreeder/NewMatingFragment.java
+++ app/src/main/java/com/example/birdbreeder/NewMatingFragment.java
@@ -1,18 +1,19 @@
 package com.example.birdbreeder;
 
 import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import androidx.fragment.app.Fragment;
-
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link NewsFeedFragment#newInstance} factory method to
+ * Use the {@link NewMatingFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class NewsFeedFragment extends Fragment {
+public class NewMatingFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -23,7 +24,7 @@ public class NewsFeedFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public NewsFeedFragment() {
+    public NewMatingFragment() {
         // Required empty public constructor
     }
 
@@ -33,11 +34,11 @@ public class NewsFeedFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment NewsFeedFragment.
+     * @return A new instance of fragment NewMatingFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static NewsFeedFragment newInstance(String param1, String param2) {
-        NewsFeedFragment fragment = new NewsFeedFragment();
+    public static NewMatingFragment newInstance(String param1, String param2) {
+        NewMatingFragment fragment = new NewMatingFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -58,6 +59,6 @@ public class NewsFeedFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_news_feed, container, false);
+        return inflater.inflate(R.layout.fragment_new_mating, container, false);
     }
 }
